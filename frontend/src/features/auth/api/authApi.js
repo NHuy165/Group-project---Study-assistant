68edf0bbf73aa46924a194edf1e9713324b3719e
Trong Tu <24122024@student.hcmus.edu.vn>
2026-06-02 10:04:34 +0700

    feat(fe): update HomePage layout, remove Hành trình học tập and hế lô ...

--- frontend/src/features/auth/api/authApi.js
+++ frontend/src/features/auth/api/authApi.js
@@ -1,21 +1,26 @@
 import axiosClient from "../../../api/axiosClient";
 // Đổi chữ username thành email ở tham số đầu vào cho đúng với UI
 export const loginUser = async (email, password) => {
-    const formData = new URLSearchParams();
-    
-    // Gắn giá trị của biến 'email' vào cái nhãn bắt buộc là 'username'
-    formData.append('username', email); 
-    formData.append('password', password);
+  const formData = new URLSearchParams();
 
-    const response = await axiosClient.post('/login', formData, {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    });
-    return response.data;
+  // Gắn giá trị của biến 'email' vào cái nhãn bắt buộc là 'username'
+  formData.append("username", email);
+  formData.append("password", password);
+
+  const response = await axiosClient.post("/login", formData, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  return response.data;
 };
 
 export const registerUser = async (userData) => {
-    const response = await axiosClient.post('/user/register', userData);
-    return response.data;
+  const response = await axiosClient.post("/user/register", userData);
+  return response.data;
+};
+
+export const getCurrentUser = async () => {
+  const response = await axiosClient.get("/user/me");
+  return response.data;
 };
