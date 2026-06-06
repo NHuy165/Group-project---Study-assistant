bea3c177d5cca2aa7e6ea9b825be4d67195f9524
NHuy165 <huyquach165@gmail.com>
2026-06-06 09:53:14 +0700

    fix(BE): Sửa endpoint read_study_assessments sai validation biến.

--- backend/src/routes/study_progress.py
+++ backend/src/routes/study_progress.py
@@ -81,18 +81,17 @@ async def read_latest_study_assessment(
 
     return result
 
+
 @router.get(
     "/study-assessment",
     response_model=StudyAssessmentOutput,
     responses={
         401: Responses.RESPONSE_401_UNAUTHORIZED,
-        404: Responses.RESPONSE_404_NOT_FOUND
+        404: Responses.RESPONSE_404_NOT_FOUND,
     },
 )
 async def read_study_assessment_by_date(
-    user: UserDep,
-    session: SessionDep,
-    specific_date: date
+    user: UserDep, session: SessionDep, specific_date: date
 ):
     result = await study_progress.read_study_assessment_by_date(
         user,
@@ -101,6 +100,7 @@ async def read_study_assessment_by_date(
     )
 
     return result
+
 
 @router.get(
     "/study-assessment/",
@@ -112,8 +112,8 @@ async def read_study_assessment_by_date(
 async def read_study_assessments(
     user: UserDep,
     session: SessionDep,
-    offset: Annotated[int | None, Field(le=0)] = None,
-    limit: Annotated[int | None, Field(le=0)] = None,
+    offset: Annotated[int | None, Field(ge=0)] = None,
+    limit: Annotated[int | None, Field(ge=0)] = None,
 ):
     result = await study_progress.read_study_assessments(
         user,
